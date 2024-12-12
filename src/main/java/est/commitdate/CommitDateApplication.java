37c4ac317d1c85fd1508398dafd7b0cc300f67a3
myh7754 <myh4755@gmail.com>
2024-12-12 14:14:35 +0900

    first commit

---
+++ src/main/java/est/commitdate/CommitDateApplication.java
@@ -0,0 +1,13 @@
+package est.commitdate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitDateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommitDateApplication.class, args);
+    }
+
+}
