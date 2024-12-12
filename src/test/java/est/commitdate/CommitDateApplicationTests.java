5a4e8da2b35fd2f139bdc9ee3cdf172e5812bb9e
myh7754 <myh4755@gmail.com>
2024-12-12 14:14:35 +0900

    first commit

---
+++ src/test/java/est/commitdate/CommitDateApplicationTests.java
@@ -0,0 +1,13 @@
+package est.commitdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitDateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
