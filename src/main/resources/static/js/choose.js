e435ce9d02e2ff17df5295f84479e9fea418d404
myh7754 <myh4755@gmail.com>
2024-12-25 18:01:18 +0900

    chat

--- src/main/resources/static/js/choose.js
+++ src/main/resources/static/js/choose.js
@@ -36,28 +36,35 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 
 
+
+
+
 function createItem(chooseDto){
 
    const itemDiv = document.createElement("div");
    itemDiv.className = "flex items-center bg-white shadow-md rounded-lg p-4";
 
    itemDiv.innerHTML = `
-    <div id="suitorId" hidden="hidden">${chooseDto.userId}</div>
-    <div class="avatar">
-      <div class="w-12 rounded-full">
-        <img id="profileImageURL" src="${chooseDto.profileImageURL}" alt="User Avatar">
-      </div>
-    </div>
-    <div class="ml-4 flex-1">
-      <p id="suitorTime" class="text-sm text-gray-500 mb-1">${chooseDto.timestamp}</p>
-      <p class="text-gray-800 font-semibold mb-1">
-        <span id="suitor" class="font-bold">${chooseDto.userName}</span>
-      </p>
-      <p id="suitorComment" class="text-gray-600 text-sm">${chooseDto.comment}</p>
-    </div>
-    <button class="btn btn-primary bg-pink-500 border-none rounded-full text-white" data-suitor-id="${chooseDto.userId}">
-      수락
-    </button>
+    <form action="/chatting" method="post">
+
+<!--         <input type="hidden" th:value="*{updatedAt}" name="updatedAt" />-->
+        <div class="avatar">
+          <div class="w-12 rounded-full">
+            <img id="profileImageURL" src="${chooseDto.profileImageURL}" alt="User Avatar">
+          </div>
+        </div>
+        <div class="ml-4 flex-1">
+          <p id="suitorTime" class="text-sm text-gray-500 mb-1">${chooseDto.timestamp}</p>
+          <p class="text-gray-800 font-semibold mb-1">
+            <span id="suitor" class="font-bold">${chooseDto.userName}</span>
+          </p>
+          <p id="suitorComment" class="text-gray-600 text-sm">${chooseDto.comment}</p>
+        </div>
+
+        <button class="btn btn-primary bg-pink-500 border-none rounded-full text-white" data-suitor-id="${chooseDto.userId}">
+          수락
+        </button>
+    </form>
   `;
 
    // 알림 리스트에 추가
