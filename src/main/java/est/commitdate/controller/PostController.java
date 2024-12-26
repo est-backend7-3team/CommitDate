03aad4c2ecd809eb2f2199da0aa3c192aad6d64f
ensoary@gmail.com <ensoary@gmail.com>
2024-12-27 00:41:43 +0900

    swipe/choose 페이지 제외 나머지 layout 적용

--- src/main/java/est/commitdate/controller/PostController.java
+++ src/main/java/est/commitdate/controller/PostController.java
@@ -1,6 +1,5 @@
 package est.commitdate.controller;
 
-
 import est.commitdate.dto.post.PostDetailDto;
 import est.commitdate.dto.post.PostDto;
 import est.commitdate.dto.post.PostUpdateDto;
@@ -15,7 +14,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Map;
 
 @Slf4j
@@ -23,107 +21,75 @@ import java.util.Map;
 @RequiredArgsConstructor
 @RequestMapping("/post")
 public class PostController {
+
     private final PostService postService;
     private final BoardService boardService;
     private final MemberService memberService;
 
+    // 게시글 목록 페이지
+    @GetMapping("")
+    public String postListView(Model model, HttpSession session) {
+        Map<String, String> loginInfo = memberService.getLoginInfo(session);
+        model.addAttribute("nickname", loginInfo.get("nickname"));
+        model.addAttribute("role", loginInfo.get("role"));
+        model.addAttribute("post", postService.PostList());
+        model.addAttribute("boards", boardService.BoardList());
+        model.addAttribute("pageTitle", "게시글 목록");
 
-    // 게시글 자세히 보기
+        // 경로 수정
+        return "view/post/posts";
+    }
+
+    // 게시글 상세 페이지
     @GetMapping("/view/{id}")
     public String postDetailView(@PathVariable Long id, Model model, HttpSession session) {
         Map<String, String> loginInfo = memberService.getLoginInfo(session);
-        model.addAttribute("nickname" , loginInfo.get("nickname"));
-        model.addAttribute("role" , loginInfo.get("role"));
-
+        model.addAttribute("nickname", loginInfo.get("nickname"));
+        model.addAttribute("role", loginInfo.get("role"));
         PostDetailDto post = PostDetailDto.from(postService.getPostById(id));
-        model.addAttribute("postDetail" , PostDetailDto.from(postService.getPostById(id)));
-        return "view/post/fragment/postDetail";
+        model.addAttribute("postDetail", post);
+
+        // 경로 수정
+        return "view/post/postDetail";
     }
 
-    // 전체 게시판 보기
-    @GetMapping("")
-    public String postView(Model model, HttpSession session) {
-        Map<String, String> loginInfo = memberService.getLoginInfo(session);
-        model.addAttribute("nickname" , loginInfo.get("nickname"));
-        model.addAttribute("role" , loginInfo.get("role"));
-        model.addAttribute("post", postService.PostList());
-        model.addAttribute("boards", boardService.BoardList());
-        model.addAttribute("pageTitle", "Fusion Platform - Main Page");
-
-        return "/view/post/layout/base";
-    }
-
-    @GetMapping("/test")
-    public String postTestView(Model model , HttpSession session) {
-        Map<String, String> loginInfo = memberService.getLoginInfo(session);
-
-        model.addAttribute("nickname" , loginInfo.get("nickname"));
-        model.addAttribute("role" , loginInfo.get("role"));
-
-        model.addAttribute("post", postService.PostList());
-        model.addAttribute("boards", boardService.BoardList());
-        model.addAttribute("pageTitle", "Fusion Platform - Main Page");
-        return "/view/post/layout/test";
-    }
-    // 해당하는 게시판의 게시글 목록 부르기
-    @GetMapping("/{id}" )
-    public String postByBoard(@PathVariable Integer id, Model model) {
-        model.addAttribute("post", postService.getPostsByBoardId(id));
-        model.addAttribute("boards", boardService.BoardList());
-        model.addAttribute("pageTitle", "Fusion Platform - Main Page");
-        return "/view/post/layout/base";
-    }
-    //게시글 생성창
+    // 게시글 생성 페이지
     @GetMapping("/saveView")
     public String postSaveView(Model model) {
         model.addAttribute("postDto", new PostDto());
         model.addAttribute("boards", boardService.BoardList());
+
         return "view/post/postSave";
     }
-    // 게시글 저장요칭
+
+    // 게시글 저장 요청
     @PostMapping("/save")
     public String postSavePost(PostDto postDto, HttpSession session) {
-
-        Member LoginMember = memberService.getLoggedInMember(session);
-        postDto.setAuthor(LoginMember.getNickname());
+        Member loggedInMember = memberService.getLoggedInMember(session);
+        postDto.setAuthor(loggedInMember.getNickname());
         postService.save(postDto);
+
         return "redirect:/post";
     }
-    // 게시글 업데이트창
+
+    // 게시글 수정 페이지
     @GetMapping("/updateView/{id}")
     public String postUpdateView(@PathVariable Long id, Model model, HttpSession session) {
-        if(postService.postAuthorizationCheck(id,session) || memberService.AuthorizationCheck(session)) {
-            model.addAttribute("postUpdateDto" , PostUpdateDto.from(postService.getPostById(id)));
+        if (postService.postAuthorizationCheck(id, session) || memberService.AuthorizationCheck(session)) {
+            model.addAttribute("postUpdateDto", PostUpdateDto.from(postService.getPostById(id)));
             return "view/post/postUpdate";
-        } else {
-            return "redirect:/post";
-        }
-    }
-    // 게시글 업데이트 요청
-    @PostMapping("/update")
-    public String postUpdatePost(PostUpdateDto postDto, HttpSession session) {
-        postService.update(postDto);
-        return "redirect:/post";
-    }
-    // 게시글 삭제 요청
-    @PostMapping("/delete/{id}")
-    public String postDeletePost(@PathVariable Long id , HttpSession session) {
-        if(postService.postAuthorizationCheck(id,session) || memberService.AuthorizationCheck(session)) {
-            postService.delete(id);
-            return "redirect:/post";
-        }
-        return "redirect:/post";
-    }
-    // 게시글 복구 요청
-    @PostMapping("/restore/{id}")
-    public String postRestore(@PathVariable Long id, HttpSession session) {
-        if(postService.postAuthorizationCheck(id,session) || memberService.AuthorizationCheck(session)) {
-            postService.restore(id);
-            return "redirect:/post";
         }
         return "redirect:/post";
     }
 
+    // 게시글 삭제 요청
+    @PostMapping("/delete/{id}")
+    public String postDeletePost(@PathVariable Long id, HttpSession session) {
+        if (postService.postAuthorizationCheck(id, session) || memberService.AuthorizationCheck(session)) {
+            postService.delete(id);
+        }
+        return "redirect:/post";
+    }
     @ResponseBody
     @PostMapping("/api/commentDelete")
     public ResponseEntity<String> postCommentDelete(@RequestBody Map<String,Object> removeJson, HttpSession session) {
@@ -139,5 +105,6 @@ public class PostController {
         return ResponseEntity.ok(postService.postCommentEdit(removeJson, session));
 
     }
-
 }
+
+
