ff8b6ccdfd95ec719c92c4ac89b75919d0897804
Philo-LEE <tlgus7777@gmail.com>
2024-12-30 20:21:28 +0900

    로우쿼리 첫글자 대문자-> 소문자로 수정 완료(배포시 문제발생Fix)

--- src/main/java/est/commitdate/repository/BoardRepository.java
+++ src/main/java/est/commitdate/repository/BoardRepository.java
@@ -12,15 +12,15 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
     Optional<Board> findByBoardId(Integer boardId);
 
     // 삭제된 데이터만 조회
-    @Query(value = "SELECT * FROM Board  WHERE status = 0", nativeQuery = true)
+    @Query(value = "SELECT * FROM board  WHERE status = 0", nativeQuery = true)
     List<Board> findDeletedBoards();
 
     // 삭제된 데이터 전부를 포함하여 조회
-    @Query(value = "SELECT * FROM Board", nativeQuery = true)
+    @Query(value = "SELECT * FROM board", nativeQuery = true)
     Optional<Board> findAllWithDeleted();
 
     // 특정 삭제된 데이터만 조회 -> :바인딩할필드이름 jpa에서 sql문에 :를 사용하면 필드와 바인딩 됨
-    @Query(value = "SELECT * FROM Board WHERE board_id = :id AND status = 0", nativeQuery = true)
+    @Query(value = "SELECT * FROM board WHERE board_id = :id AND status = 0", nativeQuery = true)
     Optional<Board> findDeleteById(Integer id);
 
 
--- src/main/java/est/commitdate/repository/PostRepository.java
+++ src/main/java/est/commitdate/repository/PostRepository.java
@@ -23,14 +23,14 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("SELECT p FROM Post p WHERE p.status = 1 ORDER BY p.createdAt DESC")
     List<Post> findAllPostsOrderedByCreatedAtDesc();
 
-    @Query(value = "SELECT * FROM Post WHERE status = 0" ,nativeQuery = true)
+    @Query(value = "SELECT * FROM post WHERE status = 0" ,nativeQuery = true)
     Optional<Post> findDeletedPosts();
 
-    @Query(value = "SELECT * FROM Post" ,nativeQuery = true)
+    @Query(value = "SELECT * FROM post" ,nativeQuery = true)
     Optional<Post> findAllWithDeleted();
 
     // 특정 삭제된 데이터만 조회 -> :바인딩할필드이름 jpa에서 sql문에 :를 사용하면 필드와 바인딩 됨
-    @Query(value = "SELECT * from Post where post_id = :id and status = 0", nativeQuery = true)
+    @Query(value = "SELECT * from post where post_id = :id and status = 0", nativeQuery = true)
     Optional<Post> findDeleteById(Long id); // 여기 id가 boardId에 바인딩 됨
 
     @Query(value = "SELECT * FROM post WHERE board_id = 1 AND status != 0 ORDER BY RAND() LIMIT 1", nativeQuery = true)
